b900c100b82e938d9d31ec6bbdbe6c65fc9a368e
mohammed.mokhtar <mohammed.mokhtar@asset.com.eg>
2025-04-28 18:45:34 +0300

    Register And Configure Martin DocumentDB

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -1,13 +1,11 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name,List<string> Category, 
         string Description, decimal Price, string ImageFile) : ICommand<CreateProductResult>;
 
     public record  CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler (IDocumentSession session )
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -22,9 +20,11 @@ namespace Catalog.API.Products.CreateProduct
             };
             //TODO
             //save the database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
 
             //return result
-            return new CreateProductResult(Guid.NewGuid());
+            return new CreateProductResult(product.Id);
         }
     }
 }
